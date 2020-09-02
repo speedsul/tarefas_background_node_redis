7b190c8297bfeb93bfc5ba02d13273b0b37f8713
speedsul <spedsul@gmail.com>
2020-09-02 00:21:51 -0300

    Conclued lesson

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import express from "express";
+import BullBoard from 'bull-board'
+import Queue from "./app/lib/Queue"
+import UserController from "./app/controllers/UserController";
+
+const app = express();
+BullBoard.setQueues(Queue.queues.map(queue => queue.bull))
+
+app.use(express.json());
+app.post("/users", UserController.store);
+app.use('/admin/queues', BullBoard.UI)
+app.listen(process.env.PORT, () => {
+  console.log(`Server Running on the port ${process.env.PORT}`);
+});
